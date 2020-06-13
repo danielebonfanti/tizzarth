dd385eaf4e99706da2b3db47c4f1ca19447e3364
Daniele Bonfanti <daniele.bonfanti@avanade.com>
2020-06-13 17:01:38 +0200

    Init repo

---
+++ app/App.svelte
@@ -0,0 +1,14 @@
+<page>
+    <actionBar title="Pizza" flat="true">
+        <actionItem on:tap="{onTapSeach}" android.systemIcon="ic_menu_search" ios.systemIcon="9" ios.position="right"/>
+    </actionBar>
+</page>
+
+<script>
+    function onTapSeach(value) {
+        
+    }
+</script>
+
+<style>
+</style>
